636163b11f90dd0f1e2f0290d2f060353125dd18
Ben North <ben@redfrontdoor.org>
2017-10-16 16:03:46 +0100

    Decoder::sample_buffer: Add field
    Initialise in ctor to suitable size for finding synchronisation phase.

--- decode-frame.cpp
+++ decode-frame.cpp
@@ -1,4 +1,6 @@
 #include <cstddef>
+#include <cstdint>
+#include <vector>
 
 struct Decoder
 {
@@ -13,4 +15,20 @@ struct Decoder
         = microseconds_per_frame * samples_per_microsecond;
 
     static constexpr size_t microseconds_phase_zero_from_vsync_rising_edge = 64;
+
+    // Space for two and a bit frames.
+    static constexpr size_t buffer_size
+        = 2 * nominal_samples_per_frame + nominal_samples_per_frame / 4;
+
+    Decoder() : sample_buffer(buffer_size)
+        {}
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    // TODO: This could be made more self-contained.  Currently each user of the
+    // buffer has to remember how many samples they've read into the buffer.  It
+    // would be better if there was a more-intelligent 'buffer' object which
+    // remembered how many samples it contained.
+    //
+    std::vector<uint8_t> sample_buffer;
 };
